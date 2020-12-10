326ecdc6b95a0eb8c3a11271a70df1e643d0dfe7
lallabah99 <56074268+lallabah99@users.noreply.github.com>
2020-12-11 00:30:21 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+
+
+//groenPijlje = document.querySelector(".groenpijltje");
+
+
+// function naarRechts() {
+// groenPijlje.classList.add("anders");
+//}
+
+// groenPijltje.addEventListener("hover", naarRechts);
+
+deP = document.querySelector("p");
+
+ deP.addEventListener("click", ietsDoen);
+ 
+function ietsDoen() {
+ deP.classList.add("anders"); 
+ 
